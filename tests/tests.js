2bb13cf082420f5f06a79676a90b531bf002f795
Tim Gallant <me@timgallant.us>
2015-12-12 13:32:12 -0700

    refactoring tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,189 +1,184 @@
-var expect = require('chai').expect;
-var Embedd = require('../src/embedd');
-var Reddit = require('../src/reddit');
+import {expect} from 'chai';
+import {decode, parseDate, embeddConstructor} from '../src/embedd';
+import {redditConstructor} from '../src/reddit';
+import {hnConstructor} from '../src/hn';
 
-var url = 'https://www.eff.org/deeplinks/2015/10/closing-loopholes-europes-net-neutrality-compromise';
-var e = new Embedd('http://canihazip.com/s');
-var r = new Reddit(url);
+const url = 'https://www.eff.org/deeplinks/2015/10/closing-loopholes-europes-net-neutrality-compromise';
 
-describe('Embedd.get', function() {
+function isPromise(x) {
+	return x instanceof Promise;
+};
 
-	it('should throw an error if no URL is specified', function() {
-		var getTest = function() {
-			return e.get();
-		};
-		expect(getTest).to.throw('No URL has been specified');
-	});
-	
-	it('should return a promise', function() {
-		var instanceTest = e.data instanceof Promise;
-		expect(instanceTest).to.equal(true);
-	});
+function isBoolean(x) {
+	return typeof x === 'boolean';
+};
 
-	it('should yield an XHR', function() {
-		e.data.then(function(data) {
-			var instanceTest = data instanceof XMLHttpRequest;
-			expect(instanceTest).to.equal(true);
-		});
-	});
+function verifyComments(obj) {
+	return !!(obj.comments && obj.score && obj.threads);
+};
 
-	it('response should not be undefined', function() {
-		e.data.then(function(data) {
-			var hasResponse = data.response !== undefined;
-			expect(hasResponse).to.equal(true);
-		});
-	});
-	
-});
-
-describe('Embedd.decode', function() {
+describe('decode', () => {
 
 	function escapeHtml(str) {
-    var div = document.createElement('div');
+    let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
+		
     return div.innerHTML;
 	};
 	
-	it('should return valid html', function() {
-		var str = '<h1>hello</h1>';
-		var encoded = escapeHtml(str);
-		var html = e.decode(escapeHtml(str));
+	it('should return valid html', () => {
+		let str = '<h1>hello</h1>',
+				encoded = escapeHtml(str),
+				html = decode(escapeHtml(str));
+		
 		expect(html).to.equal(str);
 	});
 
-	it('should return false if passed an empty string', function() {
-		var html = e.decode('');
+	it('should return false if passed an empty string', () => {
+		let html = decode('');
 		expect(html).to.equal(false);
 	});
 
-	it('should return false if passed a falsey value', function() {
-		var html = e.decode(false);
+	it('should return false if passed a falsey value', () => {
+		let html = decode(false);
 		expect(html).to.equal(false);
 	});
 	
 });
 
-describe('Embedd.parseDate', function() {
+describe('parseDate', function() {
 
-	it('should return false if no value is passed', function() {
-		var d = e.parseDate();
+	it('should return false if no value is passed', () => {
+		let d = parseDate();
 		expect(d).to.equal(false);
 	});
 
-	it('should return false if time passed is in the future', function() {
-		var now = new Date().getTime() / 1000;
-		var d = e.parseDate(now + 50);
+	it('should return false if time passed is in the future', () => {
+		let now = new Date().getTime() / 1000,
+				d = parseDate(now + 50);
+		
 		expect(d).to.equal(false);
 	});
 	
-	it('should return "a few seconds ago"', function() {
-		var now = new Date().getTime() / 1000;
-		var d = e.parseDate(now);
+	it('should return "a few seconds ago"', () => {
+		let now = new Date().getTime() / 1000,
+				d = parseDate(now);
+		
 		expect(d).to.equal('a few seconds ago');
 	});
 
-	it('should return "1 minute ago"', function() {
-		var now = new Date().getTime() / 1000;
-		var d = e.parseDate(now - 60);
+	it('should return "1 minute ago"', () => {
+		let now = new Date().getTime() / 1000,
+				d = parseDate(now - 60);
+		
 		expect(d).to.equal('1 minute ago');
 	});
 
-	it('should return "2 minutes ago"', function() {
-		var now = new Date().getTime() / 1000;
-		var d = e.parseDate(now - 120);
+	it('should return "2 minutes ago"', () => {
+		let now = new Date().getTime() / 1000,
+				d = parseDate(now - 120);
+		
 		expect(d).to.equal('2 minutes ago');
 	});
 
-	it('should return "1 hour ago"', function() {
-		var now = new Date().getTime() / 1000;
-		var d = e.parseDate(now - 3600);
+	it('should return "1 hour ago"', () => {
+		let now = new Date().getTime() / 1000,
+				d = parseDate(now - 3600);
+		
 		expect(d).to.equal('1 hour ago');
 	});
 
-	it('should return "2 hours ago"', function() {
-		var now = new Date().getTime() / 1000;
-		var d = e.parseDate(now - 7200);
+	it('should return "2 hours ago"', () => {
+		let now = new Date().getTime() / 1000,
+				d = parseDate(now - 7200);
+		
 		expect(d).to.equal('2 hours ago');
 	});
 
-	it('should return "1 day ago"', function() {
-		var now = new Date().getTime() / 1000;
-		var d = e.parseDate(now - 86400);
+	it('should return "1 day ago"', () => {
+		let now = new Date().getTime() / 1000,
+				d = parseDate(now - 86400);
+		
 		expect(d).to.equal('1 day ago');
 	});
 
-	it('should return "2 days ago"', function() {
-		var now = new Date().getTime() / 1000;
-		var d = e.parseDate(now - 172800);
+	it('should return "2 days ago"', () => {
+		let now = new Date().getTime() / 1000,
+				d = parseDate(now - 172800);
+		
 		expect(d).to.equal('2 days ago');
 	});
 	
 });
 
-describe('Reddit', function() {
-	it('should throw an error if it is passed zero values', function() {
-		var redditTest = function() {
-			return new Reddit();
+describe('embeddConstructor', () => {
+
+	it('should throw an error if no spec object is passed', () => {
+		function embeddTest() {
+			return embeddConstructor();
 		};
+		
+		expect(embeddTest).to.throw('No spec object has been specified');
+	});
+
+});
+
+describe('redditConstructor', () => {
+
+	let reddit = redditConstructor(url);
+
+	it('should throw an error if no url has been specified', () => {
+		function redditTest() {
+			return redditConstructor();
+		};
+
 		expect(redditTest).to.throw('The Reddit constructor requires a url');
 	});
-	it('should return an instance of Reddit', function() {
-		var instanceTest = r instanceof Reddit;
-		expect(instanceTest).to.equal(true);
+
+	it('should have a data property that is a promise', () => {
+		expect(isPromise(reddit.data)).to.equal(true);
 	});
 
-	it('should have a base attribute', function() {
-		expect(r.base).to.be.a('string');
+	it('should have a hasComments method that returns a boolean', () => {
+		reddit.hasComments().then(data => {
+			expect(isBoolean(data)).to.equal(true);
+		});
 	});
 
-	it('should have a data attribute', function() {
-		expect(r.data).to.be.a('promise');
+	it('should have a getComments method that returns a valid data object', () => {
+		reddit.getComments().then(data => {
+			expect(verifyComments(data)).to.equal(true);
+		});
 	});
-	
+
 });
 
-describe('Reddit.threadUrl', function() {
+describe('hnConstructor', () => {
 
-	it('should return false if passed zero values', function() {
-		var thread = r.threadUrl();
-		expect(thread).to.equal(false);
-	});
-	
-	it('should return false if passed one value', function() {
-		var thread = r.threadUrl('CrappyDesign');
-		expect(thread).to.equal(false);
-	});
+	let hn = hnConstructor(url);
 
-	it('should return false if passed two values, one being falsey', function() {
-		var thread = r.threadUrl('CrappyDesign', false);
-		expect(thread).to.equal(false);
+	it('should throw an error if no url has been specified', () => {
+		function hnTest() {
+			return hnConstructor();
+		};
+
+		expect(hnTest).to.throw('The HN constructor requires a url');
 	});
 
-	it('should return false if passed two values, both falsey', function() {
-		var thread = r.threadUrl('', '');
-		expect(thread).to.equal(false);
+	it('should have a data property that is a promise', () => {
+		expect(isPromise(hn.data)).to.equal(true);
 	});
 
-	it('should return a valid string if passed two correct values', function() {
-		var thread = r.threadUrl('RetroFuturism', '3ppebs');
-		var redditUrl = 'https://www.reddit.com/r/RetroFuturism/comments/3ppebs.json';
-		expect(thread).to.equal(redditUrl);
+	it('should have a hasComments method that returns a boolean', () => {
+		hn.hasComments().then(data => {
+			expect(isBoolean(data)).to.equal(true);
+		});
 	});
-	
-});
 
-describe('Reddit.getThreads', function() {
-
-	it('should return an array of XHR requests', function(done) {
-		r.data
-			.then(r.getThreads.bind(r))
-			.then(function(data) {
-				var xhrFilter = data.filter(function(d) {
-					return d instanceof XMLHttpRequest;
-				});
-				expect(data.length).to.equal(xhrFilter.length);
-			})
-			.then(done, done);
+	it('should have a getComments method that returns a valid data object', () => {
+		hn.getComments().then(data => {
+			expect(verifyComments(data)).to.equal(true);
+		});
 	});
-	
+
 });
