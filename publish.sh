03518127480e8ced64aceb9c73206381af232cec
Tim Gallant <me@timgallant.us>
2016-09-26 01:47:58 -0700

    adds cache clear on publish

--- publish.sh
+++ publish.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+
+# deploy to AWS
 LOCATION=embedd.io/embedd.min.js
 FILE=dist/embedd.min.js
 aws s3 cp $FILE s3://$LOCATION --region us-west-2
+
+# Clear Cloudflare Cache
+curl -X DELETE "https://api.cloudflare.com/client/v4/zones/${CF_ZONE}/purge_cache" \
+     -H "X-Auth-Email: ${CF_EMAIL}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"files":["https://embedd.io/embedd.min.js"]}'
